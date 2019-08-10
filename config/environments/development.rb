21c5cb761ea38c23ae2f261774d45524921d796b
OrayKT <oray.kt@gmail.com>
2019-08-10 22:25:45 +0100

    Websocket => ActionCable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #
+  # TODO
+  # config.action_cable.allowed_request_origins = ['https://...']
 end
