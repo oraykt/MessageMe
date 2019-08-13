b53e2014433b8dfb68c05ab08c7cb9740a28a25a
OrayKT <oray.kt@gmail.com>
2019-08-13 14:08:05 +0100

    Does it work on production?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.allowed_request_origins = ['https://kt-messageme.herokuapp.com']
+  config.action_cable.url = 'wss://kt-messageme.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+      'https://kt-messageme.herokuapp.com', 'http://kt-messageme.herokuapp.com']
 end
